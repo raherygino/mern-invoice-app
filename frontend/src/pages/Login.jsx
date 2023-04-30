81fab117e1c3f521195a658d937f8a3061086328
gino <georgino197@gmail.com>
2023-04-30 19:15:32 +0200

    Sign up user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,63 @@ import Input from "../components/form/Input"
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
 import { Link } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 const Login = () => {
+    
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
 
     return(
         <>
@@ -12,16 +67,20 @@ const Login = () => {
                 <Link to="../register"> Create Account</Link>
             </Card.Text>
             
-            <form>
+            <form onSubmit={onSubmit}>
                 <Input
                     id="email"
                     type="email"
-                    label="Your email" />
+                    label="Your email"
+                    onChange={onChange} 
+                    value={email}/>
                 
                 <Input
                     id="password"
                     type="password"
-                    label="Your password" />
+                    label="Your password"
+                    onChange={onChange} 
+                    value={password} />
                     
                 <p className="text-right">
                     <Link to="../forgot" >Forgot password ?</Link>
