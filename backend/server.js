02a6754254b71e120c0920d1a70025656c945426
gino <georgino197@gmail.com>
2023-04-30 13:25:07 +0200

    init project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
