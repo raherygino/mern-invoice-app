d31881bba172057104a7b2fc5637b3ddc2ac083e
gino <georgino197@gmail.com>
2023-04-30 17:35:52 +0200

    Setup server

---
+++ backend/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    lastname: {
+      type: String,
+      required: [true, 'Please add a lastname'],
+    },
+
+    firstname: {
+      type: String,
+      required: [true, 'Please add a firstname'],
+    },
+    
+    birth_date: {
+      type: String,
+      required: [true, 'Please add a birth date'],
+    },
+    
+    birth_place: {
+      type: String,
+      required: [true, 'Please add a birth place'],
+    },
+
+    phone: {
+      type: String,
+      required: [true, 'Please add a phone'],
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Please add an aemail'],
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
@@ -18,5 +19,22 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
