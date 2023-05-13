33ab4d66c2c6a423160158bb466dc365b8cb14fc
raherygino <georgino197@gmail.com>
2023-05-13 21:41:21 +0200

    Setting up menu

---
+++ frontend/src/menu/UserMenu.js
@@ -0,0 +1,23 @@
+const UserMenu = [
+    {
+        title: "Profile",
+        icon: "address-card",
+        variant: "success",
+        to: "./profile"
+    },
+    {
+        title: "Organization",
+        icon: "building",
+        variant: "warning",
+        to: "./organization"
+    },
+    {
+        title: "Settings",
+        icon: "settings",
+        variant: "primary",
+        to: "./settings"
+    },
+
+]
+
+export default UserMenu
---
+++ frontend/src/menu/index.js
@@ -0,0 +1,10 @@
+import CategoryMenu from "./CategoryMenu";
+import UserMenu from "./UserMenu";
+
+
+const Menu = {
+    category : CategoryMenu,
+    user: UserMenu
+}
+
+export default Menu
