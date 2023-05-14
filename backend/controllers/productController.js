9a298be23bab98dedfcc17a1f73387a03ee3909f
raherygino <georgino197@gmail.com>
2023-05-14 20:34:54 +0200

    Api: Get product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,6 @@ const Product = require('../models/productModel')
 // @desc   set product
 // @route  POST /api/products
 // @access Private
-
 const setProduct = asyncHandler(async (req, res) => {
     Product.create(req.body).then((result) => {
         res.status(200).json(result)
@@ -13,6 +12,15 @@ const setProduct = asyncHandler(async (req, res) => {
     })
 })
 
+// @desc    Get categories
+// @route   GET /api/products/:organization
+// @access  Private
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({ organization: req.params.organization })
+    res.status(200).json(products)
+})
+
 module.exports = {
     setProduct,
+    getProducts,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    setProduct,
+    setProduct, getProducts,
 } = require('../controllers/productController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', protect, setProduct)
+router.get('/:organization', getProducts)
 
 module.exports = router
