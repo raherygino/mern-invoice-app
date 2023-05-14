ad259114ace56d26ba90786296a5c10d85f39ed5
raherygino <georgino197@gmail.com>
2023-05-14 20:31:23 +0200

    Set buttons actions

--- frontend/src/pages/products/ListProducts.jsx
+++ frontend/src/pages/products/ListProducts.jsx
@@ -1,11 +1,41 @@
-import BreadCrumb from "../../components/layout/BreadCrumb";
-
+import BreadCrumb from "../../components/layout/BreadCrumb"
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
 
 const ListProducts = () => {
+
+
 
     return(
         <>
             <BreadCrumb title="Products"></BreadCrumb>
+            
+            <Container className="mt-6">
+                <Card>
+                    <Card.Body>
+                        <Card.Title className='mb-5'>All products</Card.Title>
+                        <Table bordered hover size="md">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>-</td>
+                                    <td>-</td>
+                                    <td>
+                                        
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
+            </Container>
         </>
     )
 }
--- frontend/src/pages/products/modals/ListCategoryModal.jsx
+++ frontend/src/pages/products/modals/ListCategoryModal.jsx
@@ -1,7 +1,7 @@
 import FormModal from '../../../components/form/FormModal';
 import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
 import ConfirmDialog from '../../../components/form/ConfirmDialog';
+import ButtonActions from '../../../components/form/ButtonActions';
 
 const ListCategoryModal = ({ show, onHide, categories, onSuccess, onEdit }) => {
 
@@ -41,38 +41,11 @@ const ListCategoryModal = ({ show, onHide, categories, onSuccess, onEdit }) => {
                     <tr key={category._id}>
                         <td className='pt-3'>{ category.name }</td>
                         <td width={100} align='center'>
-                            <Button variant="light" onClick={() => onEdit(index)} className="btn-icon btn-sm me-2">
-								<span className="svg-icon svg-icon-success svg-icon-md">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                                            <rect x="0" y="0" width="24" height="24"/>
-                                            <path 
-                                                d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" 
-                                                fill="#000000" 
-                                                fillRule="nonzero" 
-                                                transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "/>
-                                            <rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"/>
-                                        </g>
-                                    </svg>
-								</span>
-							</Button>
-                            <Button variant="light" onClick={() => onDelete(category._id)} className="btn-icon btn-sm">
-								<span className="svg-icon svg-icon-danger svg-icon-md">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                                            <rect x="0" y="0" width="24" height="24"/>
-                                            <path 
-                                                d="M6,8 L18,8 L17.106535,19.6150447 C17.04642,20.3965405 16.3947578,21 15.6109533,21 L8.38904671,21 C7.60524225,21 6.95358004,20.3965405 6.89346498,19.6150447 L6,8 Z M8,10 L8.45438229,14.0894406 L15.5517885,14.0339036 L16,10 L8,10 Z" 
-                                                fill="#000000" 
-                                                fillRule="nonzero"/>
-                                            <path 
-                                                d="M14,4.5 L14,3.5 C14,3.22385763 13.7761424,3 13.5,3 L10.5,3 C10.2238576,3 10,3.22385763 10,3.5 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" 
-                                                fill="#000000" 
-                                                opacity="0.3"/>
-                                        </g>
-                                    </svg>
-								</span>
-							</Button>
+                            <ButtonActions
+                                onEdit={onEdit}
+                                index={index}
+                                item={category}
+                                onDelete={onDelete} />
                         </td>
                     </tr>
                 )): null}
